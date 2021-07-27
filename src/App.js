5821b1c736f7e461304dce85632e271e7a0fefd3
tomdora <47201507+tomdora@users.noreply.github.com>
2021-07-27 09:55:24 -0400

    Component Reorganization
    Components moved into more specific subdirectories depending on function.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 5, 10)
-    },
-    {
-      id: 'e2',
-      title: 'Groceries',
-      amount: 300.54,
-      date: new Date(2021, 5, 10)
-    },
-    {
-      id: 'e3',
-      title: 'Rent',
-      amount: 1000,
-      date: new Date(2021, 5, 1)
-    },
-    {
-      id: 'e4',
-      title: 'Food',
-      amount: 20.56,
-      date: new Date(2021, 5, 8)
-    }
-  ];
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'Car Insurance',
+            amount: 294.67,
+            date: new Date(2021, 5, 10)
+        },
+        {
+            id: 'e2',
+            title: 'Groceries',
+            amount: 300.54,
+            date: new Date(2021, 5, 10)
+        },
+        {
+            id: 'e3',
+            title: 'Rent',
+            amount: 1000,
+            date: new Date(2021, 5, 1)
+        },
+        {
+            id: 'e4',
+            title: 'Food',
+            amount: 20.56,
+            date: new Date(2021, 5, 8)
+        }
+    ];
 
 
 
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <Expenses expenses={expenses} />
-    </div>
-  );
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            <Expenses items={expenses} />
+        </div>
+    );
 }
 
 export default App;
