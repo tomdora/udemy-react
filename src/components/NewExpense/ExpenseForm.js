3f36825c0650548cabdea3027c77699680c0769c
tomdora <47201507+tomdora@users.noreply.github.com>
2021-07-28 10:49:49 -0400

    Update ExpenseForm.js
    Added Title, Amount, and Date handler functions and states to start adding new elements.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,49 @@
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+    const [title, setTitle] = useState("");
+    const [amount, setAmount] = useState("");
+    const [date, setDate] = useState("");
+
+
+
+    const titleHandler = event => {
+        setTitle(event.target.value);
+        console.log(event.target.value);
+    }
+    const amountHandler = event => {
+        setAmount(event.target.value);
+        console.log(event.target.value);
+    }
+    const dateHandler = event => {
+        setDate(event.target.value);
+        console.log(event.target.value);
+    }
+
+
+
     return <form>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" />
+                <input type="text"
+                    onChange={titleHandler} />
             </div>
 
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" />
+                <input type="number"
+                    min="0.01" step="0.01"
+                    onChange={amountHandler} />
             </div>
 
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31" />
+                <input type="date"
+                    min="2019-01-01"
+                    max="2022-12-31"
+                    onChange={dateHandler} />
             </div>
         </div>
 
