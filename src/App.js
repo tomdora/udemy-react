99d0d7d1d165d4d32544e617224c25043c501081
tomdora <47201507+tomdora@users.noreply.github.com>
2021-08-03 11:56:28 -0400

    Dynamic Expenses
    Expenses are now dynamically displayed, and can be added.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
+import React, { useState } from 'react';
+
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        title: 'Car Insurance',
+        amount: 294.67,
+        date: new Date(2021, 5, 10)
+    },
+    {
+        id: 'e2',
+        title: 'Groceries',
+        amount: 300.54,
+        date: new Date(2021, 5, 10)
+    },
+    {
+        id: 'e3',
+        title: 'Rent',
+        amount: 1000,
+        date: new Date(2021, 5, 1)
+    },
+    {
+        id: 'e4',
+        title: 'Food',
+        amount: 20.56,
+        date: new Date(2021, 5, 8)
+    }
+];
+
 const App = () => {
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
     const addExpenseHandler = expense => {
         console.log(expense);
+        setExpenses(prevExpenses => {
+            return [expense, ...prevExpenses]
+        });
     }
-
-
-    const expenses = [
-        {
-            id: 'e1',
-            title: 'Car Insurance',
-            amount: 294.67,
-            date: new Date(2021, 5, 10)
-        },
-        {
-            id: 'e2',
-            title: 'Groceries',
-            amount: 300.54,
-            date: new Date(2021, 5, 10)
-        },
-        {
-            id: 'e3',
-            title: 'Rent',
-            amount: 1000,
-            date: new Date(2021, 5, 1)
-        },
-        {
-            id: 'e4',
-            title: 'Food',
-            amount: 20.56,
-            date: new Date(2021, 5, 8)
-        }
-    ];
 
 
 
