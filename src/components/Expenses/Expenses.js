063cb8da9c97a2f2ed137ad806714166c14deb8d
tomdora <47201507+tomdora@users.noreply.github.com>
2021-08-03 10:17:57 -0400

    Expenses Filter Created
    Added a filter component to start sorting expenses by year, instead of listing all at once.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,46 @@
+import React, { useState } from "react";
+
 import "./Expenses.css";
-import ExpenseItem from './ExpenseItem';
 import Card from "../UI/Card"
+import ExpenseItem from './ExpenseItem';
+import ExpenseFilter from './ExpenseFilter';
 
 const Expenses = props => {
+    const [yearSelection, setYear] = useState('2021');
+
+    const filterHandler = year => {
+        setYear(year);
+    }
     return (
-        <Card className='expenses'>
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}
-            />
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}
-            />
-            <ExpenseItem
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}
-            />
-            <ExpenseItem
-                title={props.items[3].title}
-                amount={props.items[3].amount}
-                date={props.items[3].date}
-            />
-        </Card>
+        <div>
+            <Card className='expenses'>
+                <ExpenseFilter
+                    selected={yearSelection}
+                    onYearSelect={filterHandler}
+                />
+
+                <ExpenseItem
+                    title={props.items[0].title}
+                    amount={props.items[0].amount}
+                    date={props.items[0].date}
+                />
+                <ExpenseItem
+                    title={props.items[1].title}
+                    amount={props.items[1].amount}
+                    date={props.items[1].date}
+                />
+                <ExpenseItem
+                    title={props.items[2].title}
+                    amount={props.items[2].amount}
+                    date={props.items[2].date}
+                />
+                <ExpenseItem
+                    title={props.items[3].title}
+                    amount={props.items[3].amount}
+                    date={props.items[3].date}
+                />
+            </Card>
+        </div>
     );
 }
 
